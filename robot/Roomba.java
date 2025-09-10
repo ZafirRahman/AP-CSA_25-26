067b76560d1a7feed4483b2e9846e0d5d5d2cb40
ZafirRahman <zazsz12355453gd@gmail.com>
2025-09-10 22:06:20 +0000

    Sep 10

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -6,42 +6,45 @@ public class Roomba implements Directions {
 
 	// Main method to make this self-contained
 	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
 		String worldName = "robot/basicRoom.wld";
 
 		Roomba cleaner = new Roomba();
 		int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
 		System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
-
 	}
 
-	// declared here so it is visible in all the methods!
+	// Declared here so it is visible in all the methods!
 	private Robot roomba;
-
 	// You will need to add many variables!!
 
-
 	public int cleanRoom(String worldName, int startX, int startY) {
-
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
-
 		World.readWorld(worldName);
 		World.setVisible(true);
+		World.setDelay(20);
 
+		// A new Robot should be constructed and assigned to the global (instance) variable named roomba
+		this.roomba = new Robot(startX, startY, North, 100);
 
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
+		// Use the instance variable 'roomba' for all calls
+		for (int i = 0; i < 4; i++) {
+			while (roomba.frontIsClear()) {
+				roomba.move();
+			} // This was the missing brace for the while loop
 
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
-		roomba.move();
+			// Turn right
+			roomba.turnLeft();
+			roomba.turnLeft();
+			roomba.turnLeft();
+			roomba.move();
+			roomba.turnLeft();
+			roomba.turnLeft();
+			roomba.turnLeft();
+}
+		}
 
-
-		int totalBeepers = 0; // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
-		return totalBeepers;
+		int totalBeepers = 0; // Initialize a variable to hold the final count
+		// ... cleaning logic to pick up beepers and increment `totalBeepers` ...
+		
+		return totalBeepers; // Add the return statement
 	}
 }
