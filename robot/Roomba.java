e1dc2a923615580984dea5a9cb6a82e213417306
ZafirRahman <zazsz12355453gd@gmail.com>
2025-09-16 20:38:59 +0000

    sep 16th

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -6,77 +6,97 @@ public class Roomba implements Directions {
 
     // Main method to make this self-contained
     public static void main(String[] args) {
-        String worldName = "robot/basicRoom.wld";
+        String worldName = "robot/finalTestWorld2024.wld";
 
         Roomba cleaner = new Roomba();
-        int totalBeepers = cleaner.cleanRoom(worldName, 7, 6);
+        int totalBeepers = cleaner.cleanRoom(worldName, 26, 101);
         System.out.println("Roomba cleaned up a total of " + totalBeepers + " beepers.");
     }
 
-    // Declared here so it is visible in all the methods!
     private Robot roomba;
-    // You will need to add many variables!!
 
     public int cleanRoom(String worldName, int startX, int startY) {
-    World.readWorld(worldName);
-    World.setVisible(true);
-    World.setDelay(20);
+        World.readWorld(worldName);
+        World.setVisible(true);
+        World.setDelay(1);
 
-    // A new Robot should be constructed and assigned to the global (instance) variable named roomba
-    this.roomba = new Robot(startX, startY, East, 0);
+        this.roomba = new Robot(startX, startY, East, 0);
 
-    // Initialize a variable to count the beepers
-    int totalSquaresMoved = 1;
-    int totalBeepers = 0;
-    int biggestPile = 0;
-    int CurrentPile = 0;
-    int pileCount = 0;
-    // Use the instance variable 'roomba' for all calls
-    boolean moreToClean = true;
-    while (moreToClean) {
-        CurrentPile = 1;
-        while (roomba.frontIsClear())
-        {
+        int totalSquaresMoved = 1;
+        int totalBeepers = 0;
+        int biggestPile = 0;
+        int biggestPileStreet = 0;
+        int biggestPileAvenue = 0;
+        int CurrentPile;
+        int pileCount = 0;
+
+        boolean moreToClean = true;
+        while (moreToClean) {
+
+            // reset for this square
+            CurrentPile = 0;
+
+            // pick all beepers on current square
+            while (roomba.nextToABeeper()) {
+                roomba.pickBeeper();
+                CurrentPile++;
+                totalBeepers++;
+                System.out.println("Picked 1 beeper at (" + roomba.street() + ", " + roomba.avenue() + ")");
+            }
+
+            // update pile stats once per square
+            if (CurrentPile > 0) {
+                pileCount++;
+                if (CurrentPile > biggestPile) {
+                    biggestPile = CurrentPile;
+                    biggestPileStreet = roomba.street();
+                    biggestPileAvenue = roomba.avenue();
+                }
+            }
+
+            // move across the row
+            if (roomba.frontIsClear()) {
                 roomba.move();
                 totalSquaresMoved++;
+            } else {
+                // reached end of row
+                if (roomba.facingEast()) {
+                    roomba.turnLeft();
+                    if (!roomba.frontIsClear()) {
+                        moreToClean = false;
+                    } else {
+                        roomba.move();
+                        totalSquaresMoved++;
+                        roomba.turnLeft();
+                    }
+                } else { // facing west
+                    roomba.turnLeft();
+                    roomba.turnLeft();
+                    roomba.turnLeft();
+                    if (!roomba.frontIsClear()) {
+                        moreToClean = false;
+                    } else {
+                        roomba.move();
+                        totalSquaresMoved++;
+                        roomba.turnLeft();
+                        roomba.turnLeft();
+                        roomba.turnLeft();
+                    }
+                }
             }
-        while (roomba.nextToABeeper()){
-            roomba.pickBeeper();
-            CurrentPile++;
-            totalBeepers++;
-        }
-        if (CurrentPile > biggestPile){
-            biggestPile = CurrentPile;
         }
 
-        if (roomba.facingEast()){
-            roomba.turnLeft();
-            if (!roomba.frontIsClear()){
-                roomba.turnOff();
-                break;
-            }
-            roomba.move();
-            totalSquaresMoved++;
-            roomba.turnLeft();
+        System.out.println("The largest pile is " + biggestPile + " beepers at (" + biggestPileStreet + ", " + biggestPileAvenue + ").");
+        System.out.println("The area of the room is " + totalSquaresMoved);
+        System.out.println("The total number of beepers are " + totalBeepers);
+        System.out.println("The number of piles is " + pileCount);
+        if (pileCount > 0) {
+            double averagePileSize = (double) totalBeepers / pileCount;
+            System.out.println("The average pile size is " + String.format("%.2f", averagePileSize) + " beepers.");
         }
-        else{
-            roomba.turnLeft();
-            roomba.turnLeft();
-            roomba.turnLeft();
-            roomba.move();
-            totalSquaresMoved++;
-            roomba.turnLeft();
-            roomba.turnLeft();
-            roomba.turnLeft();
-        }
+        double percentDirty = (double) pileCount / totalSquaresMoved * 100;
+        System.out.println("The percent dirty is " + String.format("%.2f", percentDirty) + "%.");
+
+        return totalBeepers; 
     }
-    
-    // ... cleaning logic to pick up beepers and increment `totalBeepers` ...
-    // Add the return statement here to return the final count
-    System.out.println("The largest pile is " + biggestPile + " beepers.");
-    System.out.println("The area of the room is " + totalSquaresMoved);
-    System.out.println("The total number of beepers are " + totalBeepers);
-    System.out.println(pileCount);
-    return biggestPile;
-}
 }
