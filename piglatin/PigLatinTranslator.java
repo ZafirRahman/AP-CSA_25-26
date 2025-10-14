dbde87a140748129b22b4e06a7afedb300deedb0
ZafirRahman <zazsz12355453gd@gmail.com>
2025-10-14 21:10:10 +0000

    Oct 14th, Pig latin project start

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -16,12 +16,24 @@ public class PigLatinTranslator {
         System.out.println("  -> translate('" + input + "')");
 
         String result = "";
+        String firstletter = input.substring(0,1);
+        if(firstletter.equalsIgnoreCase("a") || firstletter.equalsIgnoreCase("e") || firstletter.equalsIgnoreCase("i") || firstletter.equalsIgnoreCase("o") || firstletter.equalsIgnoreCase("u")){
+        result = input + "ay";
+        }else{
+            String currentletter = "";
+            int amountofconsts = 0;
+            while(!currentletter.equalsIgnoreCase("a") || !currentletter.equalsIgnoreCase("e") || !currentletter.equalsIgnoreCase("i") || !currentletter.equalsIgnoreCase("o") || currentletter.equalsIgnoreCase("u")){
+            amountofconsts++;
+            currentletter = input.substring(amountofconsts-1, amountofconsts)
+            }
+            i
+        }
 
         // TODO: translate a string input, store in result.
         // The input to this function could be any English string.
         // It may be made up of many words.
         // This method must call translateWord once for each word in the string.
-        result = translateWord(input);
+        
 
         return result;
     }
