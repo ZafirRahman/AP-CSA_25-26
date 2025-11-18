2ee1cedc5856bed53e6fdf2fa650e536a9275784
ZafirRahman <zazsz12355453gd@gmail.com>
2025-11-18 21:51:35 +0000

    nov 18th card project, card class and deck creater and shuffler

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,20 @@
 package cards;
 
 public class Card {
+    private int suit;
+    private int value;
 
+    private static String[] SUITS = {"♦", "♣", "♥","♠"};
+    private static String[] VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-}
+    public Card(int suit, int value){
+        this.suit = suit;
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
+    public String toString(){
+        return "" + SUITS[suit] + VALUES[value];
+    }
+}   
--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,34 @@
 package cards;
 
+import java.util.Random;
 public class Deck {
+    private Card[] cards;
+    private int currentCard;
 
+    public Deck(){
+        cards = new Card[52];
+        currentCard = 0;
+        
+        int x = 0;
+        for(int suit = 0; suit < 4; suit++){
+            for(int value = 0; value < 13; value++){
+                cards[x] = new Card(suit,value);
+                x++;
+            }
+        }
+    }
+
+    public void shuffler(){
+        Random ran = new Random();
+
+        for(int i = cards.length - 1; i > 0; i--){
+            int j = ran.nextInt(i+1);
+            Card temporary = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temporary;
+        }
+        currentCard = 0;
+    }
+    
 
 }
