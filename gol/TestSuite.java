bee367b0807990351900464bf14c85ac672c43e2
ZafirRahman <zazsz12355453gd@gmail.com>
2025-12-12 21:23:33 +0000

    Dec 12th project finished, couldnt commit because github was blocked.

--- gol/TestSuite.java
+++ gol/TestSuite.java
@@ -10,22 +10,17 @@ public class TestSuite {
 
         GameOfLife board = new GameOfLife(5, 5);
 
-        // Test count neighbors
         int[][] data = {{ 1 }};
 
-        // Set a single cell, then check that it has zero neighbors.
         board.set(2, 2, data);
         pass &= expect(board.countNeighbors(2,2), 0, "Single live cell with zero neighbors");
 
-        // Set the middle top neighbor.
         board.set(1, 2, data);
         pass &= expect(board.countNeighbors(2,2), 1, "Single live cell with one neighbor");
 
-        // Set the bottom middle neighbor.
         board.set(3, 2, data);
         pass &= expect(board.countNeighbors(2,2), 2, "Single live cell with two neighbors");
 
-        // Test update state
         board.step();
         board.print();
         pass &= expect(board.get(2,3), 1, "Expect line to rotate");
