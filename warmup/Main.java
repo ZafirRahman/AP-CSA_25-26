75d53b6b8e8da173de2da126ad2dbf32e2cff57b
ZafirRahman <zazsz12355453gd@gmail.com>
2026-01-07 22:52:32 +0000

    Jan 7th warm up project complete

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
-
+        int fiveSum = 0;
+        int threeSum = 0;
+        int commonFactors = 0;
+        for(int i = 0; i < 1000; i++){
+            if(i % 5 == 0 && i % 3 ==0){
+                commonFactors += i;
+            }
+        }
+        for(int i = 0; i < 1000; i+=5){
+            fiveSum += i;
+        }
+        for(int i = 0; i < 1000; i+=3){
+            threeSum += i;
+        }
+        System.out.print("The sum of all the mulitples of 3 and 5 between 1000 is ");
+        System.out.println(fiveSum + threeSum - commonFactors);
     }
 }
