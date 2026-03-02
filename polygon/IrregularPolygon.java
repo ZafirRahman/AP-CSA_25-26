a31384e44cfb3e275cb39f17c25a1213cf36bcd8
ZafirRahman <zazsz12355453gd@gmail.com>
2026-03-02 22:47:31 +0000

    I finished all the methods mar 2

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -13,19 +13,32 @@ public class IrregularPolygon {
     // public methods
     public void add(Point2D.Double aPoint)
     {
-        // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint);
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+       double perimeter = 0.0;
+        for(int i = 1; i < myPolygon.size(); i++){
+            double dx = myPolygon.get(i).getX() - myPolygon.get(i-1).getX();
+            double dy = myPolygon.get(i).getY() - myPolygon.get(i-1).getY();              
+            double distance = Math.sqrt(dx*dx + dy*dy);
+            perimeter += distance;
+            }
+        double dx = myPolygon.get(0).getX() - myPolygon.get(myPolygon.size()-1).getX();
+        double dy = myPolygon.get(0).getY() - myPolygon.get(myPolygon.size()-1).getY();
+        double distance = Math.sqrt(dx*dx + dy*dy);
+        perimeter += distance;
+        return perimeter;
     }
 
-    public double area() {
-        // TODO: Calculate the area.
-        Double area = 0.0;
-        return area;
+   public double area() {
+    Double area = 0.0;
+    for(int i = 1; i < myPolygon.size(); i++){
+        area += (myPolygon.get(i-1).getX() * myPolygon.get(i).getY()) - (myPolygon.get(i).getX() * myPolygon.get(i-1).getY());
     }
+    area += (myPolygon.get(myPolygon.size()-1).getX() * myPolygon.get(0).getY()) - (myPolygon.get(0).getX() * myPolygon.get(myPolygon.size()-1).getY());
+    return Math.abs(area) / 2;
+}
 
     public void draw()
     {
@@ -36,9 +49,19 @@ public class IrregularPolygon {
             DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
             myDrawingTool.move(myPolygon.get(0).getX(), myPolygon.get(0).getY());
             for(int i = 1; i < myPolygon.size(); i++){
-                
+                double dx = myPolygon.get(i).getX() - myPolygon.get(i-1).getX();
+                double dy = myPolygon.get(i).getY() - myPolygon.get(i-1).getY();   
+                double angle = Math.toDegrees(Math.atan2(dy, dx));
+                double distance = Math.sqrt(dx*dx + dy*dy);
+                myDrawingTool.setDirection(angle);
+                myDrawingTool.forward(distance);
             }
-        
+            double dx = myPolygon.get(0).getX() - myPolygon.get(myPolygon.size()-1).getX();
+            double dy = myPolygon.get(0).getY() - myPolygon.get(myPolygon.size()-1).getY();   
+            double angle = Math.toDegrees(Math.atan2(dy, dx));
+            double distance = Math.sqrt(dx*dx + dy*dy);
+            myDrawingTool.setDirection(angle);
+            myDrawingTool.forward(distance);
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
