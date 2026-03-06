75d18bc429c15f1cb67ba1a4ee8e737ae07eb4e5
ZafirRahman <zazsz12355453gd@gmail.com>
2026-03-06 22:04:50 +0000

    Finished project mar 6

--- polygon/Main.java
+++ polygon/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String [] args)
     {
         IrregularPolygon myPolygon = new IrregularPolygon();
+        myPolygon.add(new java.awt.geom.Point2D.Double(100, 100));
+        myPolygon.add(new java.awt.geom.Point2D.Double(200, 100));
+        myPolygon.add(new java.awt.geom.Point2D.Double(200, 200));
+        myPolygon.add(new java.awt.geom.Point2D.Double(100, 200));
         myPolygon.draw();
 
         TestSuite.run();
